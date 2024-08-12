ef3a3f224e415d5dff08e104dc985c0912b452b8
michaelmoranis <michaelmoranesul@gmail.com>
2024-08-11 21:32:51 -0300

    corrigindo click do botão meus trabalhos

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,12 +3,10 @@ import { Link } from "react-router-dom";
 
 function Button() {
     return (
-        <button className="flex items-center justify-center gap-2 w-full bg-indigo-950 text-zinc-300 font-bold text-1xl p-1 rounded-lg border-2">
-             <Link to="/works">
+        <Link to='/works' className="flex items-center justify-center gap-2 w-full bg-indigo-950 text-zinc-300 font-bold text-1xl p-1 rounded-lg border-2">
                Meus trabalhos
-             </Link>
             <FaLocationArrow />
-        </button>
+        </Link>
     )
 }
 
