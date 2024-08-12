64f1ef98e7e3986244f78d8a4659ef23b3339506
michaelmoranis <michaelmoranesul@gmail.com>
2024-08-11 21:28:38 -0300

    removendo scroll lateral corrigindo classes tailwindcss

--- src/components/WorkText/index.tsx
+++ src/components/WorkText/index.tsx
@@ -7,7 +7,7 @@ interface WorkTextProps {
 
 function WorkText(text: WorkTextProps) {
   return (
-    <div className="text-zinc-400 mx-12 px-12">
+    <div className="text-zinc-400 flex flex-col md:flex-row items-center justify-between w-full  sm:w-2/3 md:w-4/3">
       <div className="w-80 text-left">
         <p className=" w-10 border-b-4 border-b-zinc-500 text-zinc-300">
           {text.yearStudent}
