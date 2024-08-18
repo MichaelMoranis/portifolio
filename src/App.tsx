a421a63b0e9b85fd10cb9d9a664e94325a79b2a2
michaelmoranis <michaelmoranesul@gmail.com>
2024-08-18 08:40:17 -0300

    adicionando efeito de transiçao maior nos componentes com framer motion

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,10 @@ function App() {
   return (
     <motion.div
       className="box"
-      initial={{ opacity: 0, scale: 0.3 }}
+      initial={{ opacity: 0, scale: 0.5 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{
-        duration: 0.3,
+        duration: 0.8,
         delay: 0.5,
         ease: [0, 0.71, 0.2, 1.01],
       }}
--- src/pages/images.tsx
+++ src/pages/images.tsx
@@ -12,7 +12,7 @@ function Images() {
       initial={{ opacity: 0, scale: 0.5 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{
-        duration: 0.3,
+        duration: 0.8,
         delay: 0.5,
         ease: [0, 0.71, 0.2, 1.01],
       }}
