0a237bd189c70657c2bcc6b1b7e85f0a89bfdf50
michaelmoranis <michaelmoranesul@gmail.com>
2024-08-24 17:01:53 -0300

    removendo hover do menubar

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -20,13 +20,13 @@ function NavigationBar() {
       </button>
       {isOpen && (
           <ul className=" absolute h-48 right-2 text-white text-xl bg-purple-900 font-bold font-regular w-44 sm:w-60  flex flex-col justify-between items-end rounded-md z-50">
-            <li className="hover:underline hover:bg-zinc-600 hover:text-zinc-300 rounded-full p-2 m-2">
+            <li className="hover:underline hover:text-zinc-300 rounded-full p-2 m-2">
               <Link to="/works">Projetos</Link>
             </li>
-            <li className="hover:underline hover:bg-zinc-600 hover:text-zinc-300 rounded-full p-2 m-2">
+            <li className="hover:underline hover:text-zinc-300 rounded-full p-2 m-2">
             <Link to="/images">images</Link>
             </li>
-            <li className="hover:underline hover:bg-zinc-600 hover:text-zinc-300 rounded-full p-2 m-2">
+            <li className="hover:underline hover:text-zinc-300 rounded-full p-2 m-2">
             <Link  to="/setup">Itens Pc</Link>
             </li>
           </ul>
