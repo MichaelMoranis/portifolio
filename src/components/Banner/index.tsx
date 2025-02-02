4350a28a47473f0a51d58f24e6f71212c7a61c68
michaelmoranis <michaelmoranesul@gmail.com>
2025-02-02 16:02:59 -0300

    fix: correção banner responsivo

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -3,10 +3,10 @@ import Bar from "../Bar";
 
 function Banner() {
   return (
-    <div className="flex flex-col items-center content-center justify-between mt-10 md:mt-10 md:w-full lg:w-full">
-      <div className="w-full pb-4">
+    <div className="flex flex-col items-center content-center md:mt-10 md:w-full lg:w-full">
+      <div className="flex items-center pb-4">
         <img
-          className="rounded-md w-96"
+          className="rounded-md w-96 md:w-full md:h-96 "
           src={imgBanner}
           loading="lazy" height="300px" width="300px"
           alt="imagem de mesa homeoffice"
