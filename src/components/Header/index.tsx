00fa52dfa5d53636e3752f81e8857f834304de5b
michaelmoranis <michaelmoranesul@gmail.com>
2025-02-16 11:05:32 -0300

    style: change style header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,69 +1,107 @@
 import sunIcon from "../../assets/sun.png";
 import moon from "../../assets/moon-2.png";
 import { Link } from "react-router-dom";
-import burguerMenu from "../../assets/hambuger.png"
+import burguerMenu from "../../assets/hambuger.png";
 import closeMenu from "../../assets/close.png";
 import { Dispatch, SetStateAction } from "react";
 
 type HeaderProps = {
-    onMenuClick: () => void;
-    sidebarOpen: boolean;
-    darkMode: boolean
-    setDarkMode: Dispatch<SetStateAction<boolean>>
+  onMenuClick: () => void;
+  sidebarOpen: boolean;
+  darkMode: boolean;
+  setDarkMode: Dispatch<SetStateAction<boolean>>;
 };
 
-export default function Header({ sidebarOpen, onMenuClick, darkMode, setDarkMode }: HeaderProps) {
-    function toggleDisplayMode() {
-        const newMode = !darkMode ? "dark" : "light";
-        setDarkMode(!darkMode);
-        localStorage.setItem("displayMode", newMode);
-    }
+export default function Header({
+  sidebarOpen,
+  onMenuClick,
+  darkMode,
+  setDarkMode,
+}: HeaderProps) {
+  function toggleDisplayMode() {
+    const newMode = !darkMode ? "dark" : "light";
+    setDarkMode(!darkMode);
+    localStorage.setItem("displayMode", newMode);
+  }
 
-    return (
-        <header
-            className={`flex items-center justify-center w-full max-w-[900px] mx-auto left-1/2 -translate-x-1/2 fixed top-0 z-20 p-2 ${darkMode ? "bg-zinc-400" : "bg--700"
-                } backdrop-blur-2xl`}
-        >
-            <div className={`flex items-center w-full max-w-4xl justify-between ${darkMode ? "text-black" : "text-white"}`}>
-                <h1 className="font-bold text-2xl hover:underline">
-                    <Link to="/">moranisdev</Link>
-                </h1>
-                <ul className="gap-4 font-bold text-2xl items-center flex [@media(max-width:770px)]:hidden">
-                    <li className="hover:underline">
-                        <Link to="/works">Projetos</Link>
-                    </li>
-                    <li className="hover:underline">
-                        <Link to="https://www.youtube.com/@moraniss/videos">Youtube</Link>
-                    </li>
-                    <li className="hover:underline">
-                        <Link to="/setup">Imagens</Link>
-                    </li>
-                    <li className="hover:underline">
-                        <Link to="/images">Fotos</Link>
-                    </li>
-                </ul>
-                <nav className="flex justify-center gap-2 items-center relative z-30">
-                    <button
-                        onClick={toggleDisplayMode}
-                        className="relative z-30"
-                        id="mode"
-                    >
-                        <img
-                            src={darkMode ? moon : sunIcon}
-                            className="h-8 w-8 sm:h-10 sm:w-10 md:w-12 md:h-12 rounded-full"
-                        />
-                    </button>
-                    <div>
-                        <img
-                            src={sidebarOpen ? closeMenu : burguerMenu}
-                            onClick={onMenuClick}
-                            className="[@media(min-width:770px)]:hidden h-8 w-8 md:w-12 md:h-12 rounded-full"
-                            alt="Menu"
-                            id="burguer"
-                        />
-                    </div>
-                </nav>
-            </div>
-        </header>
-    )
+  return (
+    <header
+      className={`flex items-center justify-center w-full max-w-[900px] mx-auto left-1/2 -translate-x-1/2 fixed top-0 z-20 p-2 rounded-md ${
+        darkMode ? "bg-zinc-400" : "bg--700"
+      } backdrop-blur-2xl`}
+    >
+      <div
+        className={`flex items-center w-full max-w-4xl justify-between ${
+          darkMode ? "text-black" : "text-white"
+        }`}
+      >
+        <h1 className="font-bold text-2xl hover:underline">
+          <Link to="/">moranisdev</Link>
+        </h1>
+        <ul className="gap-4 font-bold text-2xl items-center flex [@media(max-width:770px)]:hidden">
+          <li className="hover:underline">
+            <Link
+              to="/works"
+              className={
+                darkMode ? "text-black hover:text-zinc-100" : "text-white"
+              }
+            >
+              Projetos
+            </Link>
+          </li>
+          <li className="hover:underline">
+            <Link
+              to="https://www.youtube.com/@moraniss/videos"
+              className={
+                darkMode ? "text-black hover:text-zinc-100" : "text-white"
+              }
+            >
+              Youtube
+            </Link>
+          </li>
+          <li className="hover:underline">
+            <Link
+              to="/setup"
+              className={
+                darkMode ? "text-black hover:text-zinc-100" : "text-white"
+              }
+            >
+              Imagens
+            </Link>
+          </li>
+          <li className="hover:underline">
+            <Link
+              to="/images"
+              className={
+                darkMode ? "text-black hover:text-zinc-100" : "text-white"
+              }
+            >
+              Fotos
+            </Link>
+          </li>
+        </ul>
+        <nav className="flex justify-center gap-2 items-center relative z-30">
+          <button
+            onClick={toggleDisplayMode}
+            className="relative z-30"
+            id="mode"
+          >
+            <img
+              src={darkMode ? moon : sunIcon}
+              className="h-8 w-8 sm:h-10 sm:w-10 md:w-12 md:h-12 rounded-full"
+            />
+          </button>
+          <div>
+            <img
+              src={sidebarOpen ? closeMenu : burguerMenu}
+              onClick={onMenuClick}
+              className="[@media(min-width:770px)]:hidden h-8 w-8 md:w-12 md:h-12 rounded-full"
+              alt="Menu"
+              id="burguer"
+            />
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
 }
